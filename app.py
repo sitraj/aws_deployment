45406124a5d6ac73b93079403ab332436aec6cc1
Shounaki Traj <shounakitraj@example.com>
2025-06-08 07:57:36 +0200

    Update app file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World from Python!"
+    return "Hello, World from Python! This is to lear AWS deployment via GH Actions"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
